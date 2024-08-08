437b0a55af8783f4ecfddc89a97d62839fe5ce52
Sukju <tjrwn1234562@gmial.com>
2024-08-08 17:19:50 +0900

    aasd

--- tobyspring/vol1/ch2/sj2/src/test/java/com/sj2/dao/DaoInter.java
+++ tobyspring/vol1/ch2/sj2/src/test/java/com/sj2/dao/DaoInter.java
@@ -1,11 +1,14 @@
 package com.sj2.dao;
 
-import java.util.List;
 
 import javax.sql.DataSource;
 
+
+
 public interface DaoInter<K extends DataSource ,V extends UserDao> {
 
+
 	public K dataSource();
+
 	public V userDao();
 }
